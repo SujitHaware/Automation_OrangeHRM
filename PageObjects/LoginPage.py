dc0d1068d85eb65725764b522da561343f208c5d
SujitHaware <sujitkumarhaware@gmail.com>
2022-10-08 23:50:28 +0530

    Added project structure & test case

---
+++ PageObjects/LoginPage.py
@@ -0,0 +1,25 @@
+class LoginPage():
+    textbox_username_name = 'username'
+    textbox_password_name = 'password'
+    button_login_xpath = '//button[@type="submit"]'
+    dropdown_user_xpath = '//p[@class="oxd-userdropdown-name"]'
+    link_logout_linktext = 'Logout'
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def setUsername(self, username):
+        self.driver.find_element_by_name(self.textbox_username_name).send_keys(username)
+
+    def setPassword(self, password):
+        self.driver.find_element_by_name(self.textbox_password_name).send_keys(password)
+
+    def clickLogin(self):
+        self.driver.find_element_by_xpath(self.button_login_xpath).click()
+
+    def clickUser(self):
+        self.driver.find_element_by_xpath(self.dropdown_user_xpath).click()
+
+    def clickLogout(self):
+        self.driver.find_element_by_link_text(self.link_logout_linktext).click()
+
---
+++ TestCases/LoginTest.py
@@ -0,0 +1,38 @@
+import unittest
+import HtmlTestRunner
+from selenium import webdriver
+from PageObjects import LoginPage
+
+class TestLogin(unittest.TestCase):
+    baseURL = 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login'
+    username = 'Admin'
+    password = 'admin123'
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(5)
+
+    @classmethod
+    def setUpClass(cls):
+        cls.driver.get(cls.baseURL)
+        cls.driver.maximize_window()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.driver.close()
+
+    def test_login(self):
+        lp = LoginPage.LoginPage(self.driver)
+        lp.setUsername(self.username)
+        lp.setPassword(self.password)
+        lp.clickLogin()
+        act_title = self.driver.title
+        lp.clickUser()
+        lp.clickLogout()
+        self.assertEqual(act_title,'OrangeHRM','Title not match')
+
+if __name__ == '__main__':
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='..\\Reports'))
+
+
+
+
+
